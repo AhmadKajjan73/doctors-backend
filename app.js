2748daedf52303c28027cdb2075e4c70bf350ad5
Ahmad-Kajjan <blaketear@hotmail.com>
2020-06-17 12:38:14 -0700

    wish it ends now

--- app.js
+++ app.js
@@ -12,20 +12,18 @@ var cors = require("cors");
 var MONGODB_URL = process.env.MONGODB_URL;
 var mongoose = require("mongoose");
 
-  const connectDB = async () => {
-	 await mongoose.connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+  mongoose.connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
 	//don't show the log when it is test
 	if(process.env.NODE_ENV !== "test") {
-			console.log("Connected to %s", MONGODB_URL);
-			console.log("App is running ... \n");
-			console.log("Press CTRL + C to stop the process. \n");
-		}
-	})
-		.catch(err => {
-			console.error("App starting error:", err.message);
-			process.exit(1);
-		});
-}
+		console.log("Connected to %s", MONGODB_URL);
+		console.log("App is running ... \n");
+		console.log("Press CTRL + C to stop the process. \n");
+	}
+})
+	.catch(err => {
+		console.error("App starting error:", err.message);
+		process.exit(1);
+	});
 var db = mongoose.connection;
 
 var app = express();
