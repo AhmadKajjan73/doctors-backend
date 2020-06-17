8f1206ddb961885efa2fb2f248a6ecd30b6559cf
Ahmad-Kajjan <blaketear@hotmail.com>
2020-06-17 03:26:35 -0700

    remove stuff

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -5,7 +5,6 @@ const { sanitizeBody } = require("express-validator");
 const apiResponse = require("../helpers/apiResponse");
 const utility = require("../helpers/utility");
 const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
 const mailer = require("../helpers/mailer");
 const { constants } = require("../helpers/constants");
 const ProductModel = require("../models/ProductModel");
