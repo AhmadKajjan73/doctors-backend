15d0c54fde6c92b1d989bb43a4bc327a6d7a7e16
AhmadKajjan73 <ahmadkajjanforgithub@gmail.com>
2021-05-24 09:07:59 +0300

    solve token jwt on heroku server by adding sameSite  param to it test3

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -169,7 +169,7 @@ exports.login = [
                   res.cookie("id_token", token, {
                     maxAge: maxAge * 1000,
                     httpOnly: true,
-                    secure: false,
+                    secure: true,
                     sameSite: "none",
                   });
                   return apiResponse.successResponseWithData(
