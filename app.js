b2196e5e724c5c525ca99edb6b381ee988c0a658
AhmadKajjan73 <ahmadkajjanforgithub@gmail.com>
2021-05-24 14:18:23 +0300

    solve token jwt on heroku server test27

--- app.js
+++ app.js
@@ -44,7 +44,18 @@ app.use(express.static(path.join(__dirname, "public")));
 //To allow cross-origin requests
 //app.use(cors());
 app.use(
-  cors({ origin: true, credentials: true, allowedHeaders: ["content-type"] })
+  cors({
+    origin: true,
+    credentials: true,
+    allowedHeaders: [
+      "Origin",
+      "X-Requested-With",
+      "Content-Type",
+      "Accept",
+      "cache-control",
+      "Authorization",
+    ],
+  })
 );
 
 //Route Prefixes
